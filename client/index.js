1e664b24501619aff4407352c81bdda0011b0f2a
Anand Simmy <anandsimmy7@gmail.com>
2021-07-27 08:38:52 +0530

    added peer connection handlers

--- client/index.js
+++ client/index.js
@@ -1,18 +1,79 @@
-const videoElement= document.getElementById('video')
+const myVideo= document.getElementById('my-video')
+const userVideo= document.getElementById('user-video')
 const startButton= document.getElementById('start')
+
+const MESSAGE_TYPE= {
+    SDP: 'SDP',
+    CANDIDATE: 'CANDIDATE',
+}
 
 startButton.addEventListener('click', async () => {
     const signaling= new WebSocket('ws://127.0.0.1:1337')
 
-    // creating peer connection
-    const peerConnection= new RTCPeerConnection({ iceServers: [{ urls: 'stun:stun.test.com:19000' }] })
-
     // adding audio and video tracks to peer connection
-    const stream= await navigator.mediaDevices.getUserMedia({ audio: true, video: true })
-    stream.getTracks().forEach(track => peerConnection.addTrack(track, stream))
-    // videoElement.srcObject= stream
-    // video.play()
+    const stream= await navigator.mediaDevices.getUserMedia({ audio: false, video: true })
+    myVideo.srcObject= stream
 
+    createPeerConnection(signaling, stream)
+})
+
+const createPeerConnection= (signaling, stream) => {
+    // creating peer connection
+    const peerConnection= new RTCPeerConnection({
+        iceServers: [{ urls: 'stun:stun.test.com:19000' }]
+    })
+
+    stream.getTracks().forEach(track => peerConnection.addTrack(track, stream))
+
+    addMessageHandler(signaling, peerConnection)
+
+    peerConnection.onnegotiationneeded= () => {
+        createAndSendOffer(signaling, peerConnection)
+    }
+
+    peerConnection.onicecandidate= (iceEvent) => {
+        signaling.send(
+            JSON.stringify({
+                message_type: MESSAGE_TYPE.CANDIDATE,
+                content: iceEvent.candidate
+        }))
+    }
+
+    peerConnection.ontrack= (event) => {
+        if(!userVideo.srcObject){
+            userVideo.srcObject= event.streams[0]
+        }
+    }
+}
+
+const addMessageHandler= (signaling, peerConnection) => {
+    signaling.onmessage= async (message) => {
+        console.log(message)
+        message= JSON.parse(message.data)
+        const { message_type, content }= JSON.parse(message.text)
+        if(message_type === MESSAGE_TYPE.SDP){
+            if(content.type === 'offer'){
+                await peerConnection.setRemoteDescription(content)
+                const answer= await peerConnection.createAnswer()
+                await peerConnection.setLocalDescription(answer)
+                signaling.send(
+                    JSON.stringify({
+                        message_type: MESSAGE_TYPE.SDP,
+                        content: answer
+                    })
+                )
+            }
+            else if(content.type ==='answer'){
+                peerConnection.setRemoteDescription(content)
+            }
+        }else if(message_type === MESSAGE_TYPE.CANDIDATE){
+            peerConnection.addIceCandidate(content)
+        }
+    }
+
+}
+
+const createAndSendOffer= async (signaling, peerConnection) => {
     // creating SDP offer (Session Description Protocol)
     const offer= await peerConnection.createOffer()
 
@@ -20,39 +81,10 @@ startButton.addEventListener('click', async () => {
     await peerConnection.setLocalDescription(offer)
 
     // sending offer
-    signaling.send(JSON.stringify({ message_type: MESSAGE_TYPE.SDP, content: offer }))
-})
-
-let connection;
-
-document.addEventListener('click', (event) => {
-    switch(event.target.id){
-        case 'connect':
-            createSocketConnection();
-            break;
-        case 'start':
-            connection.send('hello')
-            break;
-        case 'close':
-            connection.close()
-            break;
-    }
-})
-
-const createSocketConnection= () => {
-    connection= new WebSocket('ws://127.0.0.1:1337')
-
-    connection.onopen= () => {
-        connection.send('I joined')
-    }
-
-    connection.onerror= () => {
-        console.log('error occured');
-    }
-
-    connection.onmessage= (message) => {
-        data= JSON.parse(message.data)
-        console.log(`${data.client} says ${data.text}`);
-    }
-
+    signaling.send(
+        JSON.stringify({
+            message_type: MESSAGE_TYPE.SDP,
+            content: offer
+        })
+    )
 }
